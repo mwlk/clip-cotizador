3c9d7274e5bf448d2f04076e42a8ee4310bd6ab4
mwlk <mirkoivowlk@gmail.com>
2020-10-07 01:39:26 -0300

    edit label

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,12 @@ export class AppComponent {
   title = 'cotizador';
   public divizas: Array<any> = [];
   fecha = new Date();
-  hoy=this.fecha.getDate()+'/'+(this.fecha.getMonth()+1 )+'/'+this.fecha.getFullYear();
+  hoy =
+    this.fecha.getDate() +
+    '/' +
+    (this.fecha.getMonth() + 1) +
+    '/' +
+    this.fecha.getFullYear();
   constructor(private divizaService: DivizaService) {
     this.divizaService.getDiviza().subscribe((resp: any) => {
       console.log(resp);
